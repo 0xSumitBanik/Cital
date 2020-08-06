d1ed039d8d23a961e2e85741fcb6139f92697f92
Sumit Banik <sumitbanik@groveus.com>
2020-08-06 13:01:20 +0530

    Version 1 Build

--- files/js/extension.js
+++ files/js/extension.js
@@ -1,25 +1,38 @@
 document.querySelector('#submit-btn').addEventListener("click", function () {
-  let doi_url = document.getElementById('doi-url').value;
+  let doi_url = (document.getElementById('doi-url').value).trim();
+
   let citation_format = document.getElementById('select-format').value;
   let langauge = document.getElementById('select-language').value;
   let output = document.querySelector('#message');
+  let loader = document.querySelector("#loader")
 
+  if (doi_url == '') {
+    document.getElementById('doi-url').style.border='solid 2px red';
+    return;
+  }
 
+  else{
+    loader.style.visibility = 'visible'
   //API Calling
   fetch('https://citation.crosscite.org/format?doi=' + doi_url + '&style=' + citation_format + '&lang=' + langauge)
     .then(
       function (response) {
         if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
+          loader.style.visibility = 'hidden'
+          output.style.visibility = 'visible';
+          output.classList.remove("alert-success")
+          output.classList.add("alert-danger");
+          output.textContent = "DOI not found! ";
+          return
         }
 
         // Examine the text in the response
         response.text().then(function (data) {
+          loader.style.visibility = 'hidden'
           output.style.visibility = 'visible';
+          output.classList.remove("alert-danger");
           output.classList.add("alert-success")
-          output.innerHTML = data
+          output.textContent = data;
         });
       }
     )
@@ -27,5 +40,6 @@ document.querySelector('#submit-btn').addEventListener("click", function () {
       console.log('Fetch Error :-S', err);
     });
 
+  }
 
 })
